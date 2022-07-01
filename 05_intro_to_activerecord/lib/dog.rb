92a0e59163747b748e2a48f4f086b615b2b2fd5d
Connie Park <conniedc1206@gmail.com>
2022-07-01 12:31:55 -0700

    Finished code-along with lecture 5

---
+++ 05_intro_to_activerecord/lib/dog.rb
@@ -0,0 +1,7 @@
+
+class Dog < ActiveRecord::Base
+    # dog is a model
+        # a model is the part of our application that is concerned with all business logic regarding our application data
+        # this is where we shape how the data in our app is going to look like
+        # this is part that is going to communicate with the database!!
+end
