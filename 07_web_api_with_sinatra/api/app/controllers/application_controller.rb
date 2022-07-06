43dda50d8051a0b3a095b3cdf5fa9e8032140023
Connie Park <conniedc1206@gmail.com>
2022-07-06 12:35:17 -0700

    Finished code-along with lecture 7

--- 07_web_api_with_sinatra/api/app/controllers/application_controller.rb
+++ 07_web_api_with_sinatra/api/app/controllers/application_controller.rb
@@ -1,4 +1,11 @@
 class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
+  # this is where endpoints for our models are going to be defined
+  # route route: landing page
+
+  get "/" do
+    # "hello world".to_json
+    { hello: "world" }.to_json # serializing our data into JSON format
+  end
 
 end
--- 07_web_api_with_sinatra/api/app/controllers/dogs_controller.rb
+++ 07_web_api_with_sinatra/api/app/controllers/dogs_controller.rb
@@ -1,4 +1,21 @@
 class DogsController < ApplicationController
+    # create a route that returns a response of all dogs (Dog.all)
+    # index : this is a path that will return the collection of a resource
+    # follow REST conventions to create and define enpoints
+    get '/dogs' do
+        # dogs = Dog.all
+        # dogs.to_json
+        Dog.all.to_json
+    end
 
+
+    # create a dynamic route that reponses with a single dog given the ID parameter
+    # how is the id supplied? :id is dynamic/acts like a placeholder for the value
+    # how do we access the id?
+    # params: receive params through a dynamic route OR any data was submitted through a form
+    get '/dogs/:id' do
+        dog = Dog.find(params[:id])
+        dog.to_json
+    end
 
 end
